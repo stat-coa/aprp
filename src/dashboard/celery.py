8366084e80207950440d4352cd7924ca6c6009ca
TimyLian <timy117@gmail.com>
2024-05-16 12:09:44 +0800

    Updated celery settings

--- src/dashboard/celery.py
+++ src/dashboard/celery.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from celery.schedules import crontab
+from django.conf import settings
 import datetime
 
 
@@ -20,6 +21,8 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
+app.conf.enable_utc = False
+app.conf.update(timezone=settings.TIME_ZONE)
 app.conf.beat_schedule = {
     # ======================================== Job ========================================
     'monitor_profile_active_update': {

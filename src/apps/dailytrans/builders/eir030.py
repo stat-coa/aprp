92a6c66d26e992eac46a217b8680f4205c2643a0
TimyLian <timy117@gmail.com>
2024-05-30 10:32:43 +0800

    catch the exception from getting data from api

--- src/apps/dailytrans/builders/eir030.py
+++ src/apps/dailytrans/builders/eir030.py
@@ -107,8 +107,12 @@ class Api(AbstractApi):
                             columns=['上價', '中價', '下價', '平均價', '交易量', '交易日期', '作物代號', '市場名稱',
                                      '種類代碼'])
         data = data[data['作物代號'].isin(self.target_items)]
-        if not data.empty:
-            self._access_data_from_api(data)
+
+        try:
+            if not data.empty:
+                self._access_data_from_api(data)
+        except Exception as e:
+            self.LOGGER.exception(f'exception: {e}, response: {response.text}', extra=self.LOGGER_EXTRA)
 
     def _access_data_from_api(self, data: pd.DataFrame):
         data_merge = self._compare_data_from_api_and_db(data)
@@ -120,7 +124,7 @@ class Api(AbstractApi):
         if not data_merge[condition].empty:
             for _, value in data_merge[condition].fillna('').iterrows():
                 try:
-                    existed_tran = DailyTran.objects.get(id=value['id'])
+                    existed_tran = DailyTran.objects.get(id=int(value['id'] or 0))
                     if value['avg_price_x']:
                         self._update_data(value, existed_tran)
                     else:

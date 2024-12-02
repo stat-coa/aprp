27e6c43f34141710eb9dba56e47bbfdce85f4249
TimyLian <timy117@gmail.com>
2024-12-02 15:58:32 +0800

    Add testing for `WholeSaleApi05` instance and`_compare_data_from_api_and_db` method

--- src/tests/dailytrans/builders/fixtures.py
+++ src/tests/dailytrans/builders/fixtures.py
@@ -2,11 +2,33 @@ import json
 
 import environ
 import pytest
+from django.core.management import call_command
+
+from apps.crops.models import Crop
+from apps.dailytrans.builders.eir030 import Api as WholeSaleApi05
+from apps.dailytrans.builders.utils import DirectData
 
 BASE_DIR = environ.Path(__file__) - 3
 
 
 @pytest.fixture
-def crops_wholesale05():
+def load_fixtures():
+    call_command('loaddata', 'configs-type-test.yaml', verbosity=0)
+    call_command('loaddata', 'configs-unit-test.yaml', verbosity=0)
+    call_command('loaddata', 'configs-config-test.yaml', verbosity=0)
+    call_command('loaddata', 'configs-source-test.yaml', verbosity=0)
+    call_command('loaddata', 'configs-abstractproduct-crops-test.yaml', verbosity=0)
+    call_command('loaddata', 'dailytrans-cog05-test.yaml', verbosity=0)
+
+
+@pytest.fixture
+def crops_wholesale05_api():
     with open(BASE_DIR('fixtures/crops-api-wholesale05.json'), encoding='utf8') as f:
         return json.load(f)
+
+
+@pytest.fixture
+def mock_wholesale_api05(load_fixtures, crops_wholesale05_api):
+    data = DirectData('COG05', 1, 'LOT-crops')
+
+    return WholeSaleApi05(model=Crop, **data._asdict())
--- src/tests/dailytrans/builders/test_eir030.py
+++ src/tests/dailytrans/builders/test_eir030.py
@@ -1,18 +1,66 @@
+import pandas as pd
 import pytest
-from django.core.management import call_command
 
-
-@pytest.fixture
-def load_fixtures():
-    call_command('loaddata', 'configs-type-test.yaml', verbosity=0)
-    call_command('loaddata', 'configs-unit-test.yaml', verbosity=0)
-    call_command('loaddata', 'configs-config-test.yaml', verbosity=0)
-    call_command('loaddata', 'configs-source-test.yaml', verbosity=0)
-    call_command('loaddata', 'configs-abstractproduct-config5-test.yaml', verbosity=0)
-    call_command('loaddata', 'dailytrans-cog05-test.yaml', verbosity=0)
+from datetime import date
+from apps.configs.models import Config, Source, AbstractProduct
+from apps.crops.models import Crop
+from apps.dailytrans.models import DailyTran
+from apps.crops.builder import WholeSaleApi05
 
 
 @pytest.mark.django_db
-class TestEir030:
-    def test_api_wholesale05(self, crops_wholesale05, load_fixtures):
-        assert crops_wholesale05 is not None
+class TestCropsEir030:
+    @staticmethod
+    def _get_result(crops_wholesale05_api, mock_wholesale_api05) -> pd.DataFrame:
+        data = pd.DataFrame(
+            crops_wholesale05_api,
+            columns=[
+                '上價',
+                '中價',
+                '下價',
+                '平均價',
+                '交易量',
+                '交易日期',
+                '作物代號',
+                '市場名稱',
+                '種類代碼',
+            ],
+        )
+        data = data[data['作物代號'].isin(mock_wholesale_api05.target_items)]
+        return mock_wholesale_api05._compare_data_from_api_and_db(data)
+
+    def test_instance(self, mock_wholesale_api05):
+        # Arrange
+        config = Config.objects.get(code='COG05')
+        sources = Source.objects.filter(configs__exact=config).filter(type__id=1)
+        products = AbstractProduct.objects.filter(type__id=1, track_item=True)
+
+        # Assert
+        assert mock_wholesale_api05.MODEL == Crop
+        assert mock_wholesale_api05.CONFIG == config
+        assert mock_wholesale_api05.SOURCE_QS.count() == sources.count()
+        assert mock_wholesale_api05.PRODUCT_QS.count() == products.count()
+
+    def test_compare_data_from_api_and_db_with_daily_trans_existed(
+            self, mock_wholesale_api05: WholeSaleApi05, crops_wholesale05_api
+    ):
+        # Act
+        df_result = self._get_result(crops_wholesale05_api, mock_wholesale_api05)
+
+        # Assert
+        assert type(df_result.date[0]) == date
+        assert df_result.date[0].isoformat() == '2024-11-25'
+        assert df_result.query('id.notnull()').shape[0] == DailyTran.objects.filter(date=df_result.date[0]).count()
+
+    def test_compare_data_from_api_and_db_with_daily_trans_not_exist(
+            self, mock_wholesale_api05: WholeSaleApi05, crops_wholesale05_api
+    ):
+        # Arrange
+        DailyTran.objects.filter(date='2024-11-25').delete()
+
+        # Act
+        df_result = self._get_result(crops_wholesale05_api, mock_wholesale_api05)
+
+        # Assert
+        assert df_result.query('id.notnull()').shape[0] == 0
+        assert df_result.query('date.notnull()').shape[0] == len(crops_wholesale05_api)

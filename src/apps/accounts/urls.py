c67f145a3b9bc79fb2fdea8c1a389c3f432f10ad
RDNNNNN <z2715674@gmail.com>
2025-05-19 11:20:31 +0800

    add urls.py password_change url

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -1,16 +1,11 @@
 from django.conf.urls import url
-from .views import (
-    login_view,
-    logout_view,
-    register_view,
-    forgot_password_view,
-    reset_password_view,
-    user_activate,
-    reset_email_view,
-    activation_resend_view,
-)
+from . import views
+
 
 urlpatterns = [
-    url(r'^login/', login_view, name='login'),
-    url(r'^logout/', logout_view, name='logout'),
+    url(r'^password_change/$', views.change_password, name='password_change'),
+    url(r'^login/', views.login_view, name='login'),
+    url(r'^logout/', views.logout_view, name='logout'),
+    url(r'^logout/', views.logout_view, name='logout'),
+    url(r'^password_change/done/$',views.change_password_done, name='password_change_done')
 ]

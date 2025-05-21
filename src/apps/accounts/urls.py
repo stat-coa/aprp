0285536cfaaaf448411c5be1513bcbbdf96f19fa
RDNNNNN <z2715674@gmail.com>
2025-05-21 10:45:39 +0800

    remove url

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     url(r'^login/', views.login_view, name='login'),
     url(r'^logout/', views.logout_view, name='logout'),
     url(r'^password_change/$', views.change_password, name='password_change'),
-    url(r'^password_change/done/$',views.change_password_done, name='password_change_done')
 ]

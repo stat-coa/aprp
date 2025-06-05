8c1acdee17d8975659872f7333acca67a8a6559b
RDNNNNN <z2715674@gmail.com>
2025-06-05 15:20:09 +0800

    add 金鑽鳳梨(批發)

--- src/apps/dailytrans/reports/dailyreport.py
+++ src/apps/dailytrans/reports/dailyreport.py
@@ -417,6 +417,7 @@ class DailyReportFactory(object):
         self._extract_data(107, Flower, 60068, Source.objects.filter(id__in=[30001, 30002, 30003, 30004, 30005]),
                            self.specify_day)
 
+        # 金鑽鳳梨(批發 台北一, 台北二)
         self._extract_data(91, Fruit, 50068, Source.objects.filter(id__in=[20001, 20002]), self.specify_day)
 
         # TODO: 新增 '寶島梨' 至 row 56

dcce4d01af09b7240772c9e289b66d35f571ed15
RDNNNNN <z2715674@gmail.com>
2025-06-16 11:27:31 +0800

    update field

--- src/apps/dailytrans/reports/dailyreport.py
+++ src/apps/dailytrans/reports/dailyreport.py
@@ -364,10 +364,6 @@ class DailyReportFactory(object):
 
         monitor_list = list(MonitorProfile.objects.filter(watchlist=watchlist, row__isnull=False))
         
-        for mp in monitor_list:
-            if mp.row >= 70:
-                mp.row += 3
-
         for item in monitor_list:
             query_set = DailyTran.objects.filter(product__in=item.product_list())
 

6e8587ea58e5b5137d69edfb9169bf90636d8d20
RDNNNNN <z2715674@gmail.com>
2025-06-17 08:47:49 +0800

    remove 寶島甘露梨

--- src/apps/dailytrans/reports/dailyreport.py
+++ src/apps/dailytrans/reports/dailyreport.py
@@ -443,20 +443,20 @@ class DailyReportFactory(object):
             self.specify_day
             )
 
-        # 金鑽鳳梨(產地)
-        self._extract_data(
-            76, Fruit, 59008, Source.objects.filter(type_id=2), self.specify_day
-        )
+        # # 金鑽鳳梨(產地)
+        # self._extract_data(
+        #     76, Fruit, 59008, Source.objects.filter(type_id=2), self.specify_day
+        # )
         
         # 金鑽鳳梨(批發, 台北一&台北二)
         self._extract_data(
             77, Fruit, 50068, Source.objects.filter(id__in=[20001, 20002]), self.specify_day
             )
         
-        # 寶島甘露梨(批發, 東勢鎮)
-        self._extract_data(
-            56, Fruit, 50299, Source.objects.filter(id=20008), self.specify_day
-        )
+        # # 寶島甘露梨(批發, 東勢鎮)
+        # self._extract_data(
+        #     56, Fruit, 50299, Source.objects.filter(id=20008), self.specify_day
+        # )
         
         # 愛文芒果(產地)
         self._extract_data(
